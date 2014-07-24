34d63655bf657e5a8c290831ab83ed8990c2054a
jgabry <jgabry@Jonahs-MacBook-Pro.local>
2014-07-24 19:37:40 -0400

    Fixed plots in TimeSeries, added PureTimeSeries

--- Code/Regression Analysis of Temporal Processes/09_PureTimeSeries.R
+++ Code/Regression Analysis of Temporal Processes/09_PureTimeSeries.R
@@ -1,10 +1,10 @@
-#=========================#
-#====== TIME SERIES ======#
-#=========================#
+#==============================#
+#====== PURE TIME SERIES ======#
+#==============================#
 
 # Author: Jonah Gabry (jsg2201@columbia.edu)
 # Written using R version 3.1.1 on Mac OS X 10.9.3
-# Last Edited: 07/19/2014
+# Last Edited: 07/24/2014
 
 
 
@@ -17,7 +17,7 @@ setwd("INSERT PATH TO DIRECTORY")
 
 #load packages
 library(QMSS)
-library(ggplot2)
+library(gglot2)
 library(plyr)
 library(car)
 
@@ -38,6 +38,7 @@ vars <- c("cohort", "confinan", "attend", "pray", "conclerg", "homosex", "year",
           "babies", "preteen", "teens", "wrkstat", "divorce", "degree")
 
 sub <- GSS[, vars]
+
 
 # Recodes (using mutate from plyr, but could also use within(sub, ) )
 ?mutate
@@ -66,12 +67,12 @@ vars2 <- c("conclerg", "n.confinan", "fulltime", "attend", "pray", "degreelt50",
 by.year <- aggregate(sub[, vars2], list(year = sub$year), mean, na.rm = T)
 
 # interpolate for some missing years
-  # add the extra years
+# add the extra years
 by.year[30:40, "year"] <- c(1979, 1981, 1992, 1995, seq(1997, 2009, 2))
 by.year <- arrange(by.year, year)
-  # make a time series object by.year.ts
+# make a time series object by.year.ts
 by.year.ts <- ts(by.year)
-  # interpolate using na.approx
+# interpolate using na.approx
 by.year.ts <- na.approx(by.year.ts)
 
 # calculate pct strong republican, percent fulltime, percent under 50 with BA
@@ -86,7 +87,11 @@ by.year.ts <- mutate(by.year.ts,
 by.year.ts <- ts(by.year.ts, start = 1, end = which(by.year.ts$year == 1992)) 
 
 
-### plots ###
+
+
+# Time series plots -------------------------------------------------------
+# _________________________________________________________________________
+
 
 install.packages("reshape2")
 
@@ -115,12 +120,12 @@ plot.dat <- meltMyTS(mv.ts.object = by.year.ts,
 
 
 ggMyTS <- function(df, varlist, ...){
-  # varlist = character vector with names of variables to use
   require(ggplot2)
+  # varlist = character vector with names of variables to use
   include <- with(df, variable %in% varlist)
   gg <- ggplot(df[include,], aes(time, value, colour = variable)) 
   gg <- gg + geom_line(size = 1.25, ...) + geom_point(size = 3, ...)
-  gg
+  gg + theme(legend.position = "bottom")
 } 
 
 ggMyTS(df = plot.dat, varlist = c("fulltime", "n.confinan"))
@@ -129,6 +134,8 @@ ggMyTS(plot.dat, "fulltime_pct", color = "purple4") + ylab("Percent Full-time")
 ggMyTS(plot.dat, "repub_pct", color = "orangered") + ylab("Percent Strong Republican")
 ggMyTS(plot.dat, "degreelt50_pct", color = "royalblue") + ylab("Percent under 50 with BA")
 ggMyTS(plot.dat, c("n.confinan", "fulltime_pct", "repub_pct", "degreelt50_pct"))
+
+
 
 
 # correlations
@@ -143,6 +150,9 @@ ggcorr(cor.dat, palette = "RdYlBu", label = T, label_round = 2)
 
 
 
+# Heteroskedasticity & autocorrelation ------------------------------------
+# _________________________________________________________________________
+
 # simplest regression
 lm.confinan <- lm(n.confinan ~ fulltime_pct, data = by.year.ts)
 summary(lm.confinan)
@@ -153,35 +163,67 @@ bptest(lm.confinan)
 # look for autocorrelation in errors
 e <- lm.confinan$resid
 acf(e) 
-acf(e, xlim = c(1,8), col = "red", lwd = 2) # can also customize acf output
+acf(e, xlim = c(1,8), col = "red", lwd = 2, ci.type = "ma") # can also customize acf output
 plot(e) # plot residuals over time
 dwtest(lm.confinan) # Durbin-Watson test
 bgtest(lm.confinan) # Breusch-Godfrey test
 durbinWatsonTest(lm.confinan, max.lag=3) # Durbin-Watson with more lags
 
 
-# include year trend
-lm.confinan2 <- update(lm.confinan, ~ . + year)
+
+# Stationarity ------------------------------------------------------------
+# _________________________________________________________________________
+
+# stationarity: mean
+Lag.confinan <- lag(by.year.ts[,"n.confinan"])
+mean.confinan <- rowMeans(cbind(Lag.confinan, by.year.ts[,"n.confinan"]))
+mean.dat <- data.frame(year = by.year.ts[,"year"], mean = mean.confinan[-1])
+(ggplot(mean.dat, aes(x=year, y=mean)) 
+ + geom_line(color="forestgreen", size=1.25) 
+ + geom_point(size = 3))
+
+# stationarity: standard deviation
+sd.confinan <- apply(cbind(Lag.confinan, by.year.ts[,"n.confinan"]),1,sd,na.rm=T)
+sd.dat <- data.frame(year = by.year.ts[,"year"], sd = sd.confinan[-1])
+ggplot(sd.dat, aes(x=year, y=sd)) + geom_bar(stat = "identity") 
+ggplot(sd.dat, aes(x=year, y=sd)) + geom_bar(aes(fill = sd),stat = "identity") 
+
+
+
+# Weak dependence ---------------------------------------------------------
+# _________________________________________________________________________
+
+par(mfrow = c(1,3))
+varlist <- c("n.confinan", "degreelt50_pct", "repub_pct")
+clrs <- c("forestgreen", "skyblue", "orangered")
+for(i in 1:length(varlist)){
+  acf(by.year.ts[,varlist[i]], lag.max = 8, na.action = na.omit, 
+      col = clrs[i], lwd = 2, ci.type = "ma", main = varlist[i])
+}
+par(mfrow = c(1,1))
+
+# always include year trend
+lm.confinan2 <- lm(n.confinan ~ year + fulltime_pct, data = by.year.ts)
 summary(lm.confinan2)
 
-# look for autocorrelation
+# look for autocorrelation in errors
 e2 <- lm.confinan2$resid
-acf(e2, xlim = c(1,8), col = "red", lwd = 2)
-plot(e2)
-dwtest(lm.confinan2)
-bgtest(lm.confinan2)
-durbinWatsonTest(lm.confinan2, max.lag=3)
+acf(e2, xlim = c(1,8), col = "red", lwd = 2, ci.type = "ma") 
+plot(e2) # plot residuals over time
+dwtest(lm.confinan2) # Durbin-Watson test
+bgtest(lm.confinan2) # Breusch-Godfrey test
+durbinWatsonTest(lm.confinan2, max.lag=3) # Durbin-Watson with more lags
 
-
-# add some more predictores
+# add more predictors
 lm.confinan3 <- update(lm.confinan2, ~ . + repub_pct + degreelt50_pct)
 summary(lm.confinan3)
 vif(lm.confinan3) # variance inflation factor 
 durbinWatsonTest(lm.confinan3, max.lag=2)
 
-
 # We don't seem to have serial correlation here, but what if we did?
-  # Solution 1: use Newey & West autocorrelation consistent covariance matrix estimator
+
+  # Solution 1: use Newey & West autocorrelation consistent covariance matrix
+  # estimator
 coeftest(lm.confinan3, vcov = NeweyWest(lm.confinan3, lag = 1))
 
   # Solution 2: use the first differences
@@ -196,9 +238,9 @@ lm.confinan4 <- update(lm.confinan3, data = by.yearFD)
 summary(lm.confinan4)
 e4 <- lm.confinan4$resid
 plot(e4)
-acf(e4, xlim = c(1,6), col = "red", lwd = 2)
+acf(e4, xlim = c(1,6), col = "red", lwd = 2, ci.type = "ma")
 
-  # Solution 3: Feasible GLS regression, i.e., Prais-Winston or Cochrane-Orcutt
+# Solution 3: Feasible GLS regression, i.e., Prais-Winston or Cochrane-Orcutt
 # install.packages("orcutt")
 library("orcutt")
 cochrane.orcutt(lm.confinan3)
